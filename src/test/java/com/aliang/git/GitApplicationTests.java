31d1fd2f7efecf36eb9dc178a2c04cfa917299ef
aliang <372158049@qq.com>
2021-03-17 17:28:31 +0800

    第一次尝试

---
+++ src/test/java/com/aliang/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliang.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
