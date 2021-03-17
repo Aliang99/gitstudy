016439abf59ebf0f735990f150708c4c607760f0
aliang <372158049@qq.com>
2021-03-17 17:39:36 +0800

    添加了第一个控制器

---
+++ src/main/java/com/aliang/git/web/TestController.java
@@ -0,0 +1,19 @@
+package com.aliang.git.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: ALiang
+ * @Date: 2021/3/17 17:38
+ * @Description:
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test(){
+        return "Hello world";
+    }
+}
