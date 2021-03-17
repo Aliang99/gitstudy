08994010cf22cd1eb35b2245196d013291929061
aliang <372158049@qq.com>
2021-03-17 17:44:46 +0800

    添加了第三个控制器

---
+++ src/main/java/com/aliang/git/web/TestController3.java
@@ -0,0 +1,18 @@
+package com.aliang.git.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: ALiang
+ * @Date: 2021/3/17 17:38
+ * @Description:
+ */
+@RestController
+public class TestController3 {
+
+    @GetMapping("test")
+    public String test(){
+        return "Hello world";
+    }
+}
